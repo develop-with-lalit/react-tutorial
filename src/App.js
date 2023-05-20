2f26cb821f42f7ea9967984400c466325b00fcd1
develop-with-lalit <develop.with.lalit@gmail.com>
2023-05-20 14:31:26 +0530

    basic react examples

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import Todo from "./components/Todo";
 
 function App() {
   return (
-    <div className="App">
-      Hello React!!
+    <div>
+      <h1>My Todos</h1>
+      <Todo text="Learn React" />
+      <Todo text="Master React" />
+      <Todo text="Complete the course" />
     </div>
   );
 }
