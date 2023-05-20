7a47806d87a20d47b2fab1031d8ce5d5b4e55454
develop-with-lalit <develop.with.lalit@gmail.com>
2023-05-20 17:12:57 +0530

    http rquest get and post

--- src/pages/AllMeetups.js
+++ src/pages/AllMeetups.js
@@ -1,32 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import MeetupList from "../components/meetups/MeetupList";
 
-const DUMMY_DATA = [
-  {
-    id: "m1",
-    title: "This is a first meetup",
-    image:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg",
-    address: "Meetupstreet 5, 12345 Meetup City",
-    description:
-      "This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!",
-  },
-  {
-    id: "m2",
-    title: "This is a second meetup",
-    image:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg",
-    address: "Meetupstreet 5, 12345 Meetup City",
-    description:
-      "This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!",
-  },
-];
-
 const AllMeetupsPage = () => {
+  const [meetupList, setMeetupList] = useState([]);
+  const [loading, setloading] = useState();
+  useEffect(() => {
+    setloading(true);
+    fetch(
+      "https://next-react-summary-default-rtdb.asia-southeast1.firebasedatabase.app/meetups.json"
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        const meetupData = Object.keys(data).map((key) => {
+          return {
+            id: key,
+            ...data[key],
+          };
+        });
+        // console.log(meetupData);
+        setMeetupList(meetupData);
+        setloading(false);
+      });
+  }, []);
+
   return (
     <section>
       <h1> All Meetups </h1>
-      <MeetupList meetups={DUMMY_DATA} />
+      {loading && <p>Loading</p>}
+      {!loading && <MeetupList meetups={meetupList} />}
     </section>
   );
 };
--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,7 +1,29 @@
 import React from "react";
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
+import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 const NewMeetupPage = () => {
-  return <div>NewMeetupPage</div>;
+  const history = useHistory();
+  function handleAddMeetup(meetupData) {
+    fetch(
+      "https://next-react-summary-default-rtdb.asia-southeast1.firebasedatabase.app/meetups.json",
+      {
+        method: "POST",
+        body: JSON.stringify(meetupData),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    ).then(() => {
+      history.replace("/");
+    });
+  }
+  return (
+    <section>
+      <h1>Add New Meetup</h1>
+      <NewMeetupForm onAddMeetup={handleAddMeetup} />
+    </section>
+  );
 };
 
 export default NewMeetupPage;
